36f83343443c8bf0e82c9f47e36b4342f610afa6
ArGwaster <148948313+ArGwaster@users.noreply.github.com>
2024-12-13 17:08:00 +0100

    hpp

---
+++ Header.hpp
@@ -0,0 +1,59 @@
+#ifndef CINEMA_HEADER_HPP
+#define CINEMA_HEADER_HPP
+
+using namespace std;
+#include <iostream>
+class Cinema
+{
+public:
+	Cinema();
+	~Cinema();
+
+private:
+
+};
+
+Cinema::Cinema()
+{
+}
+
+Cinema::~Cinema()
+{
+}
+
+class Movie
+{
+public:
+	Movie();
+	~Movie();
+
+private:
+
+};
+
+Movie::Movie()
+{
+}
+
+Movie::~Movie()
+{
+}
+
+class Tciket
+{
+public:
+	Tciket();
+	~Tciket();
+
+private:
+
+};
+
+Tciket::Tciket()
+{
+}
+
+Tciket::~Tciket()
+{
+}
+#endif
